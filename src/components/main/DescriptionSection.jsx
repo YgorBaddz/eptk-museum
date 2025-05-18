1167c6b209964b22ab5f50868a2ad494ed60978b
Ygor Badmayev <igorbadmaev2002@gmail.com>
2025-05-19 01:01:05 +0300

    lil fix

--- src/components/main/DescriptionSection.jsx
+++ src/components/main/DescriptionSection.jsx
@@ -1,13 +1,13 @@
 function DescriptionSection() {
   return (
-    <section className="flex items-center justify-center border-b-2 border-[#CF9056] px-4 py-12">
+    <section className="flex items-center justify-center border-b-2 border-[#E67E22] px-4 py-12">
       <h4 className="max-w-[600px] text-center text-xl lg:text-3xl">
-        Мы <span className="text-[#CF9056]">сохраняем </span>и{" "}
-        <span className="text-[#CF9056]">передаем </span> будущим поколениям
-        память о <span className="text-[#CF9056]">педагогах</span>,{" "}
-        <span className="text-[#CF9056]">студентах </span>и важных{" "}
-        <span className="text-[#CF9056]">событиях</span> колледжа. <br />
-        Наш музей - это живая <span className="text-[#CF9056]">
+        Мы <span className="text-[#E67E22]">сохраняем </span>и{" "}
+        <span className="text-[#E67E22]">передаем </span> будущим поколениям
+        память о <span className="text-[#E67E22]">педагогах</span>,{" "}
+        <span className="text-[#E67E22]">студентах </span>и важных{" "}
+        <span className="text-[#E67E22]">событиях</span> колледжа. <br />
+        Наш музей - это живая <span className="text-[#E67E22]">
           история{" "}
         </span>{" "}
         технического образования и человеческих достижений.
